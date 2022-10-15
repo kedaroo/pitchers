2b300c249a16dea8264da6240b9c95dbd8bbf660
Kedar Basutkar <krb1202@gmail.com>
2022-10-15 18:27:44 +0530

    add: getPitchByUserId service

--- backend/services/pitchServices.js
+++ backend/services/pitchServices.js
@@ -5,3 +5,7 @@ exports.CREATE_PITCH = `INSERT INTO pitches
 exports.GET_ALL_PITCHES = `SELECT BIN_TO_UUID(id) AS id, 
     BIN_TO_UUID(user_id) AS user_id, category, caption, 
     secure_url, created_at FROM pitches;`;
+
+exports.GET_PITCHES_BY_USER_ID = `SELECT BIN_TO_UUID(id) AS id, 
+    BIN_TO_UUID(user_id) AS user_id, category, caption, 
+    secure_url, created_at FROM pitches WHERE user_id = UUID_TO_BIN(?);`;
