c7f8709cf2d59b9c031f36e2c1f44e60f714e6ba
Kedar Basutkar <krb1202@gmail.com>
2022-10-15 16:09:53 +0530

    add: user services

--- backend/services/userServices.js
+++ backend/services/userServices.js
@@ -1,3 +1,10 @@
 exports.CREATE_USER = `INSERT INTO users 
-    (id, name, email, role, profile_picture) 
-    VALUES (UUID_TO_BIN(?), ?, ?, ?, ?);`;
+    (id, email, role) 
+    VALUES (UUID_TO_BIN(?), ?, ?);`;
+
+exports.GET_USER_BY_EMAIL = `SELECT * from users WHERE email = ?;`;
+
+exports.UPDATE_USER = `UPDATE users SET bio = ?, startup = ?, name = ?, 
+    profile_picture = ? WHERE id = UUID_TO_BIN(?);`;
+
+exports.UPDATE_ROLE = `UPDATE users SET role = ? WHERE id = UUID_TO_BIN(?);`;
