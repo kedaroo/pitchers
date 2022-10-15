2f036c60ef899c5bfa5759cae77fc59442d876e5
priyanshk20 <khandelwalpriyansh85@gmail.com>
2022-10-15 22:40:16 +0530

    added chatbox container

---
+++ frontend/src/components/Chatbox/SingleBox.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './SingleBox.css'
+const SingleBox = () => {
+  return (
+    <>
+            <div className="mssg">
+                <div className="avatar">A</div>
+                <div className="mssg-text">its a dummy mssg....</div>
+            </div>
+    </>
+  )
+}
+
+export default SingleBox
