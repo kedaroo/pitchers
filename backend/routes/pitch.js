e3662b15252503ce56b4d8cc352e332d2d05a559
Kedar Basutkar <krb1202@gmail.com>
2022-10-15 18:28:24 +0530

    add: getPitchesByUserId route

--- backend/routes/pitch.js
+++ backend/routes/pitch.js
@@ -5,9 +5,11 @@ const auth = require("../middleware/auth");
 const {
   createPitch,
   getAllPitches,
+  getPitchesByUserId,
 } = require("../controllers/pitchController");
 
 router.route("/createPitch").post(auth, createPitch);
 router.route("/getAllPitches").get(auth, getAllPitches);
+router.route("/getPitchesByUserId/:user_id").get(auth, getPitchesByUserId);
 
 module.exports = router;
