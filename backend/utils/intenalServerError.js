28e199e7b812e98fdba9688e448f61742dfb2e19
Kedar Basutkar <krb1202@gmail.com>
2022-10-15 16:16:34 +0530

    fix:

--- backend/utils/intenalServerError.js
+++
@@ -1,8 +0,0 @@
-const internalServerError = (res) => {
-  return res.status(500).json({
-    success: false,
-    message: "Internal server error. Please try again!",
-  });
-};
-
-module.exports = internalServerError;
