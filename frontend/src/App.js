a100cef7250667c620fac6da0b5e7a1b92d4118b
Hinanshi Suthar <hinanshis4@gmail.com>
2022-10-15 20:08:54 +0530

    [Feat] User profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,40 @@
 import "./App.css";
-import { LoginForm } from "./components/LoginForm";
+import { SignupForm } from "./components/form/SignupForm";
 import "../src/output.css";
 import { Route, Routes } from "react-router-dom";
+import { UserProfile } from "./pages/UserProfile";
+import { LoginForm } from "./components/form/LoginForm";
+import { useEffect } from "react";
+import { useAuth } from "./context/auth-context";
+import { useNavigate } from "react-router-dom";
+import { BottomBar } from "./components/BottomBar";
+import { MyProfile } from "./pages/MyProfile";
 
 function App() {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/profile");
+    }
+    navigate("/");
+  }, []);
+
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-      </Routes>
+    <div className="grid place-items-center">
+      <div className="wrapper">
+        <div className="wrapper grid place-items-center">
+          <Routes>
+            <Route path="/" element={<SignupForm />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/profile" element={<MyProfile />} />
+          </Routes>
+          <div className="bottom-0 fixed">
+            <BottomBar />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
