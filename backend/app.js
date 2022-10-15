2130dbc9ed5c4ad9ac8b3803af13b578300fc4dd
Kedar Basutkar <krb1202@gmail.com>
2022-10-15 17:02:52 +0530

    config: express-fileupload

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+app.use(fileUpload());
 
 const user = require("./routes/user");
 
